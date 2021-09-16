45aac3f9d9f3ce08652e3018fc8038d0f9f1e81f
totoguri <finalyo4@naver.com>
2021-09-16 18:56:58 +0900

    java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,71 @@
-//class Main {
+//class SwitchBreak {
 //    public static void main(String[] args) {
-//	    int num1;
-//        num1 = 10;
+//        int n = 3;
 //
-//        int num2 = 20;
-//        int num3 = num1 + num2;
-//        System.out.println(num1 + " + " + num2 + " = " + num3);
+//        switch(n) {
+//            case 1:
+//                System.out.println("Simple Java");
+//                break;
+//            case 2:
+//                System.out.println("Funny Java");
+//                break;
+//            case 3:
+//                System.out.println("Fantastic Java");
+//                break;
+//            default:
+//                System.out.println("The best programming language");
+//        }
+//        System.out.println("Do you like Java?");
 //    }
 //}
 
 
-class Constatnts {
-    public static void main(String[] args) {
-        final int MAX_SIZE = 100;
-        final char CONST_CHAR = 'U';
-        final int CONST_ASSIGNED;
+//class MethodReturns {
+//    public static void main(String[] args) {
+//        int result;
+//        result = adder(4, 5);
+//        System.out.println("4 + 5 = " +result);
+//        System.out.println("3.5 x 3.5 = " + square(3.5));
+//    }
+//
+//    public static int adder(int num1, int num2) {
+//        return num1 + num2;
+//    }
+//
+//    public static double square(double num) {
+//        return num * num;
+//    }
+//}
 
-        CONST_ASSIGNED = 12;
-        System.out.println("1 : " + MAX_SIZE);
-        System.out.println("2 : " + CONST_CHAR);
-        System.out.println("3 : " + CONST_ASSIGNED);
+
+class BankAccount {
+    int balance = 0;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+    public void checkMyBalance() {
+        System.out.println("잔액 : " + balance);
     }
 }
+
+class Bank {
+    public static void main(String[] args) {
+        BankAccount yoon = new BankAccount();
+        BankAccount park = new BankAccount();
+
+        yoon.deposit(5000);
+        park.deposit(3000);
+
+        yoon.withdraw(2000);
+        park.withdraw(2000);
+
+        yoon.checkMyBalance();
+        park.checkMyBalance();
+    }
+}
+
+// p.163
