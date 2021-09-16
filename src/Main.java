27ab53e2162b11b5a7a51de36a79cc78c92e67a0
totoguri <finalyo4@naver.com>
2021-09-16 18:29:48 +0900

    java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
-public class Main {
+//class Main {
+//    public static void main(String[] args) {
+//	    int num1;
+//        num1 = 10;
+//
+//        int num2 = 20;
+//        int num3 = num1 + num2;
+//        System.out.println(num1 + " + " + num2 + " = " + num3);
+//    }
+//}
 
+
+class Constatnts {
     public static void main(String[] args) {
-	// write your code here
+        final int MAX_SIZE = 100;
+        final char CONST_CHAR = 'U';
+        final int CONST_ASSIGNED;
+
+        CONST_ASSIGNED = 12;
+        System.out.println("1 : " + MAX_SIZE);
+        System.out.println("2 : " + CONST_CHAR);
+        System.out.println("3 : " + CONST_ASSIGNED);
     }
 }
