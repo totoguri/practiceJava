dea700d176026c8b00355044e37bf7f427d47d8e
totoguri <finalyo4@naver.com>
2021-09-21 20:28:08 +0900

    java

--- src/Main.java
+++ src/Main.java
@@ -396,38 +396,531 @@
 //}
 
 
-class Man {
-    String name;
-
-    public Man(String name) {
-        this.name = name;
-    }
-    public void tellYourName() {
-        System.out.println("My name is " + name);
-    }
-}
-
-class BusinessMan extends Man {
-    String company;
-    String position;
-
-    public BusinessMan(String name, String company, String position) {
-        super(name);
-        this.company = company;
-        this.position = position;
-    }
-    public void tellYourInfo() {
-        System.out.println("My company is " + company);
-        System.out.println("My position is " + position);
-        tellYourName();
-    }
-}
-
-class MyBusinessMan {
-    public static void main(String[] args) {
-        BusinessMan man = new BusinessMan("YOON", "Hybrid ELD", "Staff Eng.");
-        man.tellYourInfo();
-    }
-}
+//class Man {
+//    String name;
+//
+//    public Man(String name) {
+//        this.name = name;
+//    }
+//    public void tellYourName() {
+//        System.out.println("My name is " + name);
+//    }
+//}
+//
+//class BusinessMan extends Man {
+//    String company;
+//    String position;
+//
+//    public BusinessMan(String name, String company, String position) {
+//        super(name);
+//        this.company = company;
+//        this.position = position;
+//    }
+//    public void tellYourInfo() {
+//        System.out.println("My company is " + company);
+//        System.out.println("My position is " + position);
+//        tellYourName();
+//    }
+//}
+//
+//class MyBusinessMan {
+//    public static void main(String[] args) {
+//        BusinessMan man = new BusinessMan("YOON", "Hybrid ELD", "Staff Eng.");
+//        man.tellYourInfo();
+//    }
+//}
 
 // p.328
+
+
+//class MobilePhone {
+//    protected String number;
+//
+//    public MobilePhone(String num) {
+//        number = num;
+//    }
+//    public void answer() {
+//        System.out.println("Hi~ from " + number);
+//    }
+//}
+//
+//class SmartPhone extends MobilePhone {
+//    private final String androidVer;
+//
+//    public SmartPhone(String num, String ver) {
+//        super(num);
+//        androidVer = ver;
+//    }
+//    public void playApp() {
+//        System.out.println("App is running in " + androidVer);
+//    }
+//}
+//
+//class MobileSmartPhone {
+//    public static void main(String[] args) {
+//        SmartPhone phone = new SmartPhone("010-5555-7777", "Nougat");
+//        phone.answer();
+//        phone.playApp();
+//    }
+//}
+
+
+//class MobilePhone {
+//    protected String number;
+//
+//    public MobilePhone(String num) {
+//        this.number = num;
+//    }
+//    public void answer() {
+//        System.out.println("Hi~ from " + number);
+//    }
+//}
+//
+//class SmartPhone extends MobilePhone {
+//    private final String androidVer;
+//
+//    public SmartPhone(String num, String ver) {
+//        super(num);
+//        this.androidVer = ver;
+//    }
+//    public void playApp() {
+//        System.out.println("App is running in " + androidVer);
+//    }
+//}
+//
+//class MobileSmartPhoneRef {
+//    public static void main(String[] args) {
+//        SmartPhone ph1 = new SmartPhone("010-4444-5555", "Nougat");
+//        MobilePhone ph2 = new SmartPhone("010-3457-7901", "Nougat");
+//
+//        ph1.answer();
+//        ph1.playApp();
+//
+//        ph2.answer();
+//    }
+//}
+
+
+//class Cake {
+//    public void yummy() {
+//        System.out.println("Yummy Cake");
+//    }
+//}
+//
+//class CheeseCake extends Cake {
+//    public void yummy() {
+//        System.out.println("Yummy Cheese Cake");
+//    }
+//}
+//
+//class YummyCakeOverriding {
+//    public static void main(String[] args) {
+//        Cake c1 = new CheeseCake();
+//        CheeseCake c2 = new CheeseCake();
+//
+//        c1.yummy();
+//        c2.yummy();
+//    }
+//}
+
+
+//class UnivFriend {
+//    private final String name;
+//    private final String major;
+//    private final String phone;
+//
+//    public UnivFriend(String name, String major, String phone) {
+//        this.name = name;
+//        this.major = major;
+//        this.phone = phone;
+//    }
+//    public void showInfo() {
+//        System.out.println("이름: " + name);
+//        System.out.println("전공: " + major);
+//        System.out.println("전화: " + phone);
+//    }
+//}
+//
+//class CompFriend {
+//    private final String name;
+//    private final String department;
+//    private final String phone;
+//
+//    public CompFriend(String name, String department, String phone) {
+//        this.name = name;
+//        this.department = department;
+//        this.phone = phone;
+//    }
+//    public void showInfo() {
+//        System.out.println("이름: " + name);
+//        System.out.println("부서: " + department);
+//        System.out.println("전화: " + phone);
+//    }
+//}
+//
+//class MyFriends {
+//    public static void main(String[] args) {
+//        UnivFriend[] uFriends = new UnivFriend[5];
+//        int uCount = 0;
+//
+//        CompFriend[] cFriends = new CompFriend[5];
+//        int cCount = 0;
+//
+//        uFriends[uCount++] = new UnivFriend("LEE", "Computer", "010-3333-5555");
+//        uFriends[uCount++] = new UnivFriend("SEO", "Electronics", "010-2222-4444");
+//
+//        cFriends[cCount++] = new CompFriend("YOON", "R&D 1", "02-1234-5678");
+//        cFriends[cCount++] = new CompFriend("PARK", "R&D 2", "02-2323-3434");
+//
+//        for(int i=0; i<uCount; i++) {
+//            uFriends[i].showInfo();
+//            System.out.println();
+//        }
+//        for(int i=0; i<cCount; i++) {
+//            cFriends[i].showInfo();
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+//class Friend {
+//    protected String name;
+//    protected String phone;
+//
+//    public Friend(String name, String phone) {
+//        this.name = name;
+//        this.phone = phone;
+//    }
+//    public void showInfo() {
+//        System.out.println("이름: " + name);
+//        System.out.println("전화: " + phone);
+//    }
+//}
+//
+//class UnivFriend extends Friend {
+//    private final String major;
+//
+//    public UnivFriend(String name, String major, String phone) {
+//        super(name, phone);
+//        this.major = major;
+//    }
+//    public void showInfo() {
+//        super.showInfo();
+//        System.out.println("전공: " + major);
+//    }
+//}
+//
+//class CompFriend extends Friend {
+//    private final String department;
+//
+//    public CompFriend(String name, String department, String phone) {
+//        super(name, phone);
+//        this.department = department;
+//    }
+//    public void showInfo() {
+//        super.showInfo();
+//        System.out.println("부서: " + department);
+//    }
+//}
+//
+//class MyFriend {
+//    public static void main(String[] args) {
+//        Friend[] frns = new Friend[10];
+//        int cnt = 0;
+//
+//        frns[cnt++] = new UnivFriend("LEE", "Computer", "010-3333-4444");
+//        frns[cnt++] = new UnivFriend("SEO", "Electronics", "010-3454-4343");
+//        frns[cnt++] = new CompFriend("YOON", "R&D 1", "02-2121-1212");
+//        frns[cnt++] = new CompFriend("PARK", "R&D 2", "02-3333-4444");
+//
+//        for(int i=0; i<cnt; i++) {
+//            frns[i].showInfo();
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+//interface Printable {
+//    public void print(String doc);
+//}
+//
+//class Printer implements Printable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println(doc);
+//    }
+//}
+//
+//class PrintableInterface {
+//    public static void main(String[] args) {
+//        Printable prn = new Printer();
+//        prn.print("Hello Java");
+//    }
+//}
+
+
+//interface Printable {
+//    void print(String doc);
+//}
+//
+//interface ColorPrintable extends Printable {
+//    void printCMYK(String doc);
+//}
+//
+//class Prn909Drv implements ColorPrintable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From MD-909 black & white ver");
+//        System.out.println(doc);
+//    }
+//
+//    @Override
+//    public void printCMYK(String doc) {
+//        System.out.println("From MD-909 CMYK ver");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class Prn999Drv implements ColorPrintable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From MD-999 black & white ver");
+//        System.out.println(doc);
+//    }
+//
+//    @Override
+//    public void printCMYK(String doc) {
+//        System.out.println("From MD-999 CMYK ver");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class PrinterDriver {
+//    public static void main(String[] args) {
+//        String myDoc = "This is a report about...";
+//
+//        ColorPrintable prn = new Prn909Drv();
+//        prn.print(myDoc);
+//        prn.printCMYK(myDoc);
+//        System.out.println();
+//
+//        prn = new Prn999Drv();
+//        prn.print(myDoc);
+//        prn.printCMYK(myDoc);
+//    }
+//}
+
+
+//interface Printable {
+//    void print(String doc);
+//    default void printCMYK(String doc) {}
+//}
+//
+//class Prn731Drv implements Printable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From MD-731 printer");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class Prn909Drv implements Printable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From MD-909 black & white ver");
+//        System.out.println(doc);
+//    }
+//
+//    @Override
+//    public void printCMYK(String doc) {
+//        System.out.println("From MD-909 CMYK ver");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class PrinterDriver {
+//    public static void main(String[] args) {
+//        String myDoc = "This is a report about...";
+//        Printable prn1 = new Prn731Drv();
+//        prn1.print(myDoc);
+//        System.out.println();
+//
+//        Printable prn2 = new Prn909Drv();
+//        prn2.print(myDoc);
+//    }
+//}
+
+
+//interface Printable {
+//    static void printLine(String str) {
+//        System.out.println(str);
+//    }
+//    default void print(String doc) {
+//        printLine(doc);
+//    }
+//}
+//
+//class Printer implements Printable {}
+//
+//class SimplePrinter {
+//    public static void main(String[] args) {
+//        String myDoc = "This is a report about...";
+//        Printable prn = new Printer();
+//        prn.print(myDoc);
+//
+//        Printable.printLine("end of line");
+//    }
+//}
+
+
+//interface Printable {
+//    void printLine(String str);
+//}
+//
+//class SimplePrinter implements Printable {
+//    public void printLine(String str) {
+//        System.out.println(str);
+//    }
+//}
+//
+//class MultiPrinter extends SimplePrinter {
+//    public void printLine(String str) {
+//        super.printLine("Start of multi...");
+//        super.printLine(str);
+//        super.printLine("End of multi");
+//    }
+//}
+//
+//class InstanceofInterface {
+//    public static void main(String[] args) {
+//        Printable prn1 = new SimplePrinter();
+//        Printable prn2 = new MultiPrinter();
+//
+//        if(prn1 instanceof Printable)
+//            prn1.printLine("This is a simple printer.");
+//        System.out.println();
+//
+//        if(prn2 instanceof Printable)
+//            prn2.printLine("This is a multiful printer.");
+//    }
+//}
+
+
+//interface Upper {}
+//interface Lower {}
+//
+//interface Printable {
+//    String getContents();
+//}
+//
+//class Report implements Printable, Upper {
+//    String cons;
+//
+//    Report(String cons) {
+//        this.cons = cons;
+//    }
+//    public String getContents() {
+//        return cons;
+//    }
+//}
+//
+//class Printer {
+//    public void printContents(Printable doc) {
+//        if(doc instanceof Upper) {
+//            System.out.println((doc.getContents()).toUpperCase());
+//        }
+//        else if(doc instanceof Lower) {
+//            System.out.println((doc.getContents()).toLowerCase());
+//        }
+//        else
+//            System.out.println(doc.getContents());
+//    }
+//}
+//
+//class MarkerInterface {
+//    public static void main(String[] args) {
+//        Printer prn = new Printer();
+//        Report doc = new Report("Simple Funny News~");
+//        prn.printContents(doc);
+//    }
+//}
+
+// p.384
+
+
+//import java.util.Scanner;
+//
+//class ExceptionCase {
+//    public static void main(String[] args) {
+//        Scanner kb = new Scanner(System.in);
+//        System.out.print("a/b...a? ");
+//        int n1 = kb.nextInt();
+//        System.out.print("a/b...b? ");
+//        int n2 = kb.nextInt();
+//        System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+//        System.out.println("Good bye~~!");
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//class ExceptionCase {
+//    public static void main(String[] args) {
+//        Scanner kb = new Scanner(System.in);
+//
+//        try {
+//            System.out.print("a/b...a? ");
+//            int n1 = kb.nextInt();
+//            System.out.print("a/b...b? ");
+//            int n2 = kb.nextInt();
+//            System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+//        }
+//        catch(ArithmeticException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        System.out.println("Good Bye~~!");
+//    }
+//}
+
+
+//import java.util.Scanner;
+//import java.util.InputMismatchException;
+//
+//class ExceptionCase {
+//    public static void main(String[] args) {
+//        Scanner kb = new Scanner(System.in);
+//
+//        try {
+//            System.out.print("a/b...a? ");
+//            int n1 = kb.nextInt();
+//            System.out.print("a/b...b? ");
+//            int n2 = kb.nextInt();
+//            System.out.printf("%d / %d = %d \n", n1, n2, n1/n2);
+//        }
+//        catch(InputMismatchException | ArithmeticException e) {
+//            e.getMessage();
+//        }
+//
+//        System.out.println("Good bye");
+//    }
+//}
+
+
+class ExceptionMessage {
+    public static void md1(int n) {
+        md2(n, 0);
+    }
+    public static void md2(int n1, int n2) {
+        int r = n1/n2;
+    }
+    public static void main(String[] args) {
+        md1(3);
+        System.out.println("Good bye");
+    }
+}
+
+// p.395
