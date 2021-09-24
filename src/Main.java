0039fa809236a13a76960efda55e993e554aba47
totoguri <finalyo4@naver.com>
2021-09-24 21:53:52 +0900

    java

--- src/Main.java
+++ src/Main.java
@@ -1778,6 +1778,42 @@
 // p.552
 
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+class IteratorCollection {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("Toy");
+        list.add("Box");
+        list.add("Robot");
+        list.add("Box");
+
+        Iterator<String> itr = list.iterator();
+
+        while(itr.hasNext())
+            System.out.print(itr.next() + '\t');
+        System.out.println();
+
+        itr = list.iterator();
+
+        String str;
+        while(itr.hasNext()) {
+            str = itr.next();
+            if(str.equals("Box"))
+                itr.remove();
+        }
+
+        itr = list.iterator();
+
+        while(itr.hasNext())
+            System.out.print(itr.next() + '\t');
+        System.out.println();
+    }
+}
+
+// p.555
 
 
 
