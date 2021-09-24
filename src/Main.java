90dc94a6dede9000cafc2acdada7ab15a519f758
totoguri <finalyo4@naver.com>
2021-09-24 19:44:17 +0900

    java

--- src/Main.java
+++ src/Main.java
@@ -1752,7 +1752,30 @@
 //}
 
 
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//class ArrayListCollection {
+//    public static void main(String[] args) {
+//        List<String> list = new LinkedList<>();
+//
+//        list.add("Toy");
+//        list.add("Box");
+//        list.add("Robot");
+//
+//        for(int i = 0; i <list.size(); i++)
+//            System.out.print(list.get(i) + '\t');
+//        System.out.println();
+//
+//        list.remove(0);
+//
+//        for(int i = 0; i < list.size(); i++)
+//            System.out.print(list.get(i) + '\t');
+//        System.out.println();
+//    }
+//}
 
+// p.552
 
 
 
