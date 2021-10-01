a19dedebbfa6cf60ee93ea1a003d535a001e2107
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-01 17:42:29 +0900

    java

--- src/Block.java
+++ src/Block.java
@@ -348,76 +348,41 @@
 //}
 
 
-interface Printable {
-    public void print(String doc);
-}
-
-class SPrinterDriver implements Printable {
-    @Override
-    public void print(String doc) {
-        System.out.println("From Samsung printer");
-        System.out.println(doc);
-    }
-}
-
-class LPrinterDriver implements Printable {
-    @Override
-    public void print(String doc) {
-        System.out.println("From LG printer");
-        System.out.println(doc);
-    }
-}
-
-class PrinterDriver {
-    public static void main(String[] args) {
-        String myDoc = "This is a report about...";
-
-        Printable prn = new SPrinterDriver();
-        prn.print(myDoc);
-        System.out.println();
-
-        prn = new LPrinterDriver();
-        prn.print(myDoc);
-    }
-}
+//interface Printable {
+//    public void print(String doc);
+//}
+//
+//class SPrinterDriver implements Printable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From Samsung printer");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class LPrinterDriver implements Printable {
+//    @Override
+//    public void print(String doc) {
+//        System.out.println("From LG printer");
+//        System.out.println(doc);
+//    }
+//}
+//
+//class PrinterDriver {
+//    public static void main(String[] args) {
+//        String myDoc = "This is a report about...";
+//
+//        Printable prn = new SPrinterDriver();
+//        prn.print(myDoc);
+//        System.out.println();
+//
+//        prn = new LPrinterDriver();
+//        prn.print(myDoc);
+//    }
+//}
 
 // p.481
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// **** END of Passion Java ****
 
 
 
